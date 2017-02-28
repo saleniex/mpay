7744201e7a8687471f4ef73bbd066cef479d89d1
Uldis <uj@mobilly.lv>
2017-02-28 15:54:22 +0200

    Specialized exceptions.

---
+++ src/Mpay/RequestException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uldis
+ * Date: 17.28.2
+ * Time: 14:10
+ */
+
+namespace Mpay;
+
+
+class RequestException extends \Exception
+{
+
+}
---
+++ src/Mpay/ResponseException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uldis
+ * Date: 17.28.2
+ * Time: 15:36
+ */
+
+namespace Mpay;
+
+
+class ResponseException extends \Exception
+{
+
+}
