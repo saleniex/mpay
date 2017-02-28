409d407f67d89cf0161f61d2a04c37c2c7337e72
Uldis <uj@mobilly.lv>
2017-02-28 15:53:25 +0200

    Connector send request method implementation.

--- src/Mpay/Connector.php
+++ src/Mpay/Connector.php
@@ -8,21 +8,65 @@
 
 namespace Mpay;
 
+use GuzzleHttp\Client;
 
 class Connector
 {
-    /**
-     * @var Signer
-     */
-    private $signer;
+    const SERVICE_ENDPOINT = 'https://mpay.mobilly.lv';
 
-    public function __construct(Signer $signer)
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $serviceEndpoint;
+
+    /**
+     * @var SecurityContext
+     */
+    private $context;
+
+
+    public function __construct(SecurityContext $context, $serviceEndpoint = self::SERVICE_ENDPOINT)
     {
-        $this->signer = $signer;
+        $this->serviceEndpoint = $serviceEndpoint;
+        $this->context = $context;
     }
+
 
     public function send(Request $request)
     {
-        return new Response('{}');
+        $response = $this->getHttpClient()->post($this->serviceEndpoint, [
+            'body' => $request->getJson(),
+        ]);
+
+        $content = $response->getBody()->getContents();
+
+        if (201 != $response->getStatusCode()) {
+            return new ErrorResponse($content);
+        }
+
+        $data = json_decode($content, true);
+        $signature = $data[Response::F_SIGNATURE];
+        unset($data[Response::F_SIGNATURE]);
+
+        $signer = $this->context->getSigner();
+        if ( ! $signer->verify($data, $signature)) {
+            throw new ResponseException('Invalid response signature.');
+        }
+
+        return new SuccessResponse($content);
+    }
+
+    protected function getHttpClient()
+    {
+        if ( ! $this->client) {
+            $this->client = new Client();
+        }
+
+        return $this->client;
     }
 }
