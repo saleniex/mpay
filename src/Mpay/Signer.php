3a1f31e41b4c3fe405807d5ea0f2ed6d5aa128b7
Uldis <uj@mobilly.lv>
2017-02-28 16:10:25 +0200

    Signer implementation.

--- src/Mpay/Signer.php
+++ src/Mpay/Signer.php
@@ -11,23 +11,69 @@ namespace Mpay;
 
 class Signer
 {
+    private $privateKey;
+    private $privateKeySecret;
+    private $publicKey;
+
     public function __construct($privateKey, $privateKeySecret, $publicKey)
     {
-        
+        $this->privateKey = $privateKey;
+        $this->privateKeySecret = $privateKeySecret;
+        $this->publicKey = $publicKey;
     }
 
-    public function sign($data)
+    /**
+     * Sign data.
+     *
+     * @param array $data
+     *
+     * @return string Base64 encoded signature
+     *
+     * @throws \Exception In case of sign error.
+     */
+    public function sign(array $data)
     {
-        
+        $dataNormalizer = new SignDataNormalizer();
+
+        $signature = null;
+        $result = openssl_sign(
+            $dataNormalizer->normalize($data),
+            $signature,
+            $this->privateKey,
+            OPENSSL_ALGO_SHA256);
+
+        if ( ! $result) {
+            throw new \Exception(sprintf('Error while signing data "%s".', $data));
+        }
+
+        return base64_encode($signature);
     }
 
-    public function verify($data, $signature)
+    /**
+     * Verify signature.
+     *
+     * @param array $data
+     * @param string $signature Base64 encoded signature.
+     *
+     * @return bool TRUE if signature is valid.
+     *
+     * @throws \Exception
+     */
+    public function verify(array $data, $signature)
     {
-        
-    }
+        $dataNormalizer = new SignDataNormalizer();
 
-    protected function prepareData(array $data)
-    {
+        $binSignature = base64_decode($signature);
+        $result = openssl_verify(
+            $dataNormalizer->normalize($data),
+            $binSignature,
+            $this->publicKey,
+            OPENSSL_ALGO_SHA256);
 
+        if (0 > $result) {
+            throw new \Exception(sprintf('Error while verifying signature for "%s".', $data));
+        }
+
+        return 1 === $result;
     }
 }
