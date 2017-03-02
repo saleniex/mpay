5d78753aef02fa898ba07da7711f225bc9816e80
Uldis <uj@mobilly.lv>
2017-03-02 14:10:17 +0200

    Signer key loading.

--- src/Mobilly/Mpay/Signer.php
+++ src/Mobilly/Mpay/Signer.php
@@ -2,7 +2,6 @@
 
 namespace Mobilly\Mpay;
 
-
 /**
  * Signer.
  * @package Mobilly\Mpay
@@ -33,11 +32,16 @@ class Signer
     {
         $dataNormalizer = new SignDataNormalizer();
 
+        $privateKey = openssl_pkey_get_private('file://' . $this->privateKey, $this->privateKeySecret);
+        if (false === $privateKey) {
+            throw new \Exception(sprintf('Cannot sign. Cannot open private key "%s" with pass "%s".', $this->privateKey, $this->privateKeySecret));
+        }
+
         $signature = null;
         $result = openssl_sign(
             $dataNormalizer->normalize($data),
             $signature,
-            $this->privateKey,
+            $privateKey,
             OPENSSL_ALGO_SHA256);
 
         if ( ! $result) {
@@ -61,11 +65,16 @@ class Signer
     {
         $dataNormalizer = new SignDataNormalizer();
 
+        $publicKey = openssl_pkey_get_public('file://' . $this->publicKey);
+        if (false === $publicKey) {
+            throw new \Exception(sprintf('Cannot verify signature. Cannot open public key "%s".', $this->publicKey));
+        }
+
         $binSignature = base64_decode($signature);
         $result = openssl_verify(
             $dataNormalizer->normalize($data),
             $binSignature,
-            $this->publicKey,
+            $publicKey,
             OPENSSL_ALGO_SHA256);
 
         if (0 > $result) {
