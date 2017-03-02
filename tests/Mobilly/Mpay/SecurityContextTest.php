ee6b53b53a381d68dda43d98c5d16e73b02edd28
Uldis <uj@mobilly.lv>
2017-03-02 14:15:53 +0200

    Security context tests.

---
+++ tests/Mobilly/Mpay/SecurityContextTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uldis
+ * Date: 17.2.3
+ * Time: 14:12
+ */
+
+namespace Tests\Mobilly\Mpay;
+
+require_once 'src/Mobilly/Mpay/SecurityContext.php';
+require_once 'src/Mobilly/Mpay/Signer.php';
+
+use Mobilly\Mpay\SecurityContext;
+use Mobilly\Mpay\Signer;
+
+
+class SecurityContextTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetUser()
+    {
+        $context = new SecurityContext('user', 'private.pem', 'testkey', 'publickey');
+
+        $this->assertEquals('user', $context->getUser());
+    }
+
+    public function testGetSigner()
+    {
+        $context = new SecurityContext('user', 'private.pem', 'testkey', 'publickey');
+        $this->assertTrue($context->getSigner() instanceof Signer);
+    }
+}
